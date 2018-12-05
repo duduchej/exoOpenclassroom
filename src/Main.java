a6ca2bccb1615e8c39ab24c9e003129723729464
Junior Duchemin <junior.duchemin@outlook.com>
2018-12-05 09:33:25 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,111 @@
+import java.io.File;
+
+public class Main {
+
+    private String test;
+    private boolean test1;
+    private int test2;
+    private double test3;
+    private float test4;
+
+    public static void main(String[] args) {
+
+/*
+        Main main = new Main();
+        System.out.println(main.test);
+        System.out.println(main.test1);
+        System.out.println(main.test2);
+        System.out.println(main.test3);
+        System.out.println(main.test4);
+
+        int a,b;
+        int i=0;
+
+        a=i++;
+        b=++i;
+        System.out.println(a + " et " + b);
+
+
+        double x = 5;
+        float y = 2;
+        float z = 2.5f;   //f is mandatory
+        float z1 = 2; //f isn't mandatory
+        int d= -5;
+        //important rules!
+        //Smaller data types, namely byte, short, and char, are first promoted to int any time
+        //they’re used with a Java binary arithmetic operator, even if neither of the operands is
+        //int.
+
+        short x2 = 10;
+        short y2 = 3;
+       // short z2 = x2 * y2; // DOES NOT COMPILE
+
+
+        int x3 = 2, z3 = 3;
+        x3 = x3 * z3; // Simple assignment operator
+        x3 *= z3; // Compound assignment operator
+
+        // XOR   ou exclusif Le résultat est VRAI si un et un seul des opérandes A et B est VRAI
+
+
+
+        System.out.println(z1 < 2 ? 5: 10) ;
+
+        System.out.println(x > 2 ? x < 4 ? 10 : 8 : 7);
+        System.out.println("before");
+
+        //int div = 11/0;return arithmétic exception
+        System.out.println( x3>2 ? "Oui" : "no");  //expression ternaire dans un print!
+        System.out.println("------------------------------------");
+       final String testa="bb";
+        String choix = "non" ;
+        switch(choix)
+        {
+            default:
+                System.out.println("boom");
+
+            case testa:
+                System.out.println("non");
+
+            case "oui":
+                System.out.println("oui");
+            break;
+            case "ok" :
+                System.out.println("ok");
+
+// attention au break !   note break
+
+            break;
+
+
+        }
+
+     for(;;){    // works but not for() or for(;)
+         System.out.println("ntm");
+         break;
+     }
+     //ternaire operation
+
+
+        try {
+            System.out.println("hello");
+        }catch(Exception e)
+        {
+            System.out.println("gg");          // try doesn't require a catch if a finally is present
+        }
+
+        File f = new File("test.txt");
+        System.out.println(f.getAbsoluteFile());
+        System.out.println(f.getAbsolutePath());
+        System.out.println(f.exists());
+
+
+
+
+*/
+
+
+
+    }
+
+}
